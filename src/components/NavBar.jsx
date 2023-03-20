289e6acfb0211cfa4e24fce78ee7236b3c76b351
yohai <ohanayohaiisrael@gmail.com>
2023-03-20 13:14:02 +0200

    ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,22 @@
+//react imports
 import React from 'react'
+
+//icons imports
+import {SiThemoviedatabase} from 'react-icons/si'
+
 
 const NavBar = () => {
   return (
     <div className=' absolute w-screen flex justify-center z-10' >
-            <div className="mt-5 flex w-5/6 justify-between">
-                <div className="logo w-3/12 text-white">
-                  VOD        
+            <div className="mt-5 flex w-5/6 justify-between items-center">
+                <div className="logo w-3/12 flex">
+                  <span className='text-white text-6xl'><SiThemoviedatabase/></span>
+                </div>
+                <div className="search w-3/12 flex justify-center">
+                  <input type="text" />
+                </div>
+                <div className="menu w-3/12 flex justify-end">
+                  <span className='text-white'>cart</span>
                 </div>
             </div>
     </div>
