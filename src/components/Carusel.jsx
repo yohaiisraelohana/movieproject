716f83490899cd9e6823d4bfc02e7afcc66e5d41
yohai <ohanayohaiisrael@gmail.com>
2023-03-20 15:28:08 +0200

    ok

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -4,44 +4,44 @@ import React from 'react'
 // style imports
 import '../styles/carusel.css'
 
-const Carusel = () => {
-    const slides = [
-        {
-          title: "Machu Picchu",
-          subtitle: "Peru",
-          description: "Adventure is never far away",
-          image:
-            "https://images.unsplash.com/photo-1571771019784-3ff35f4f4277?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        },
-        {
-          title: "Chamonix",
-          subtitle: "France",
-          description: "Let your dreams come true",
-          image:
-            "https://images.unsplash.com/photo-1581836499506-4a660b39478a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        },
-        {
-          title: "Mimisa Rocks",
-          subtitle: "Australia",
-          description: "A piece of heaven",
-          image:
-            "https://images.unsplash.com/photo-1566522650166-bd8b3e3a2b4b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        },
-        {
-          title: "Four",
-          subtitle: "Australia",
-          description: "A piece of heaven",
-          image:
-            "https://images.unsplash.com/flagged/photo-1564918031455-72f4e35ba7a6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        },
-        {
-          title: "Five",
-          subtitle: "Australia",
-          description: "A piece of heaven",
-          image:
-            "https://images.unsplash.com/photo-1579130781921-76e18892b57b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        }
-      ];
+const Carusel = ({bank_movies:slides}) => {
+    // const slides = [
+    //     {
+    //       title: "Machu Picchu",
+    //       subtitle: "Peru",
+    //       description: "Adventure is never far away",
+    //       image:
+    //         "https://images.unsplash.com/photo-1571771019784-3ff35f4f4277?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+    //     },
+    //     {
+    //       title: "Chamonix",
+    //       subtitle: "France",
+    //       description: "Let your dreams come true",
+    //       image:
+    //         "https://images.unsplash.com/photo-1581836499506-4a660b39478a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+    //     },
+    //     {
+    //       title: "Mimisa Rocks",
+    //       subtitle: "Australia",
+    //       description: "A piece of heaven",
+    //       image:
+    //         "https://images.unsplash.com/photo-1566522650166-bd8b3e3a2b4b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+    //     },
+    //     {
+    //       title: "Four",
+    //       subtitle: "Australia",
+    //       description: "A piece of heaven",
+    //       image:
+    //         "https://images.unsplash.com/flagged/photo-1564918031455-72f4e35ba7a6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+    //     },
+    //     {
+    //       title: "Five",
+    //       subtitle: "Australia",
+    //       description: "A piece of heaven",
+    //       image:
+    //         "https://images.unsplash.com/photo-1579130781921-76e18892b57b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+    //     }
+    //   ];
       
       function useTilt(active) {
         const ref = React.useRef(null);
@@ -122,19 +122,19 @@ const Carusel = () => {
             <div
               className="slideBackground"
               style={{
-                backgroundImage: `url('${slide.image}')`
+                backgroundImage: `url('${slide.Poster}')`
               }}
             />
             <div
               className="slideContent"
               style={{
-                backgroundImage: `url('${slide.image}')`
+                backgroundImage: `url('${slide.Poster}')`
               }}
             >
               <div className="slideContentInner">
-                <h2 className="slideTitle">{slide.title}</h2>
-                <h3 className="slideSubtitle">{slide.subtitle}</h3>
-                <p className="slideDescription">{slide.description}</p>
+                <h2 className="slideTitle">{slide.Title}</h2>
+                <h3 className="slideSubtitle">{slide.movie}</h3>
+                <p className="slideDescription">{slide.Year}</p>
               </div>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,27 @@
 //react imports
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 
 //components imports
 import Carusel from '../components/Carusel'
 import NavBar from '../components/NavBar'
 
 const Home = () => {
+  const [bank_movies,setBankMovie] = useState([]); 
   useEffect(()=>{
     fetch('http://www.omdbapi.com/?s=bank&apikey=d628e0b4')
       .then(res=>res.json())
-      .then(data=>console.log(data))
+      .then(data=>{
+        const {Search} = data;
+        setBankMovie(Search);
+      })
       .catch(err=>console.log(err))
-  },[])
+  },[]);
+  console.log(bank_movies);
   return (
     <div>
         <NavBar/>
         <div className=' h-screen'>
-            <Carusel/>
+            <Carusel bank_movies={bank_movies}/>
         </div>
     </div>
   )
