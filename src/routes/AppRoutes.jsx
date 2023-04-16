720794f645cac4255272da154e8d1d842165ca94
yohai <ohanayohaiisrael@gmail.com>
2023-04-16 09:59:25 +0300

    ok

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,3 +1,23 @@
 //react imports
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+
+//layouts imports
+import GuestLayout from '../layouts/GuestLayout'
+
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                  <Route path="/" element={<GuestLayout/>}>
+
+                  </Route>
+                  <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Router>
+    )
+  }
+  
+  export default AppRoutes
