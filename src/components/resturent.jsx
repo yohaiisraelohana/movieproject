f1e6feff8b37923a4588a9f3d50e078bc2650ce7
yohai <ohanayohaiisrael@gmail.com>
2023-04-16 15:26:13 +0300

    ok

--- src/components/resturent.jsx
+++ src/components/resturent.jsx
@@ -19,7 +19,23 @@ const Resturent = () => {
             className='h-[30vh] w-screen'
             src={`${presented_resturent.main_image}`} alt="" />
         <div className=" w-screen flex flex-wrap justify-center h-[70vh]">
-            <div className="w-[60vw] bg-white h-fit"></div>
+            <div className="w-[80vw] bg-white h-fit z-10 -mt-[5vh] p-8 rounded-md">
+                <p className='text-3xl border-b-2' >{presented_resturent.name}</p>
+                <p>{presented_resturent.city}, {presented_resturent.cuisine} {presented_resturent.price}
+                {presented_resturent.price && new Array(5-presented_resturent.price.length).fill(`$`).map((d,i)=>(
+                 <span key={i} className=' text-gray-400' >{d}</span>
+                 ))}
+                </p>
+                <p>{presented_resturent.description}</p>
+                <div className="grid grid-cols-2">
+                    {presented_resturent.images && presented_resturent.images.map((i)=>(
+                        <img 
+                        className='h-full w-full'
+                        src={`${i}`} alt="" />
+                    ))}
+                </div>
+            </div>
+            
         </div>
     </div>
   )
