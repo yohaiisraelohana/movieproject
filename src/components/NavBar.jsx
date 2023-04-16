aa7b3adcacfc1e76662c6cee609c0a99a817f123
yohai <ohanayohaiisrael@gmail.com>
2023-04-16 17:58:32 +0300

    ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //assets imports
   //images
   import logo from '../assets/logoImage.jpeg';
   //icons
   import {AiOutlineMenu} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
-  const nav_options = ["Cart","Account","Add Resturent"];
+  const nav_options = [{name:"Add Resturent",link:"addResturent"},{name:"Manage Resturents",link:""}];
+  const [show_menu,setShowMenue] = useState(false);
   return (
     <div
         className=' absolute top-0 flex justify-center w-screen h-[10vh] bg-white border-b border-black px-[2vw]'>
         <div 
           className="flex justify-between w-full">
-          <div 
+          <Link to={'/'}
             className="w-2/6 ">
             <img 
               className='h-full'
               src={logo} alt="" />
-          </div>
+          </Link>
           <div className="w-1/6 h-full flex justify-evenly items-center">
-            <div className="md:hidden text-3xl">
+            <button 
+              onClick={()=>setShowMenue(!show_menu)}
+              className="md:hidden text-3xl">
               <AiOutlineMenu/>
-            </div>
+            </button>
+            {show_menu && 
+                <div className="md:hidden absolute top-[10vh] w-screen flex flex-wrap z-20 right-0 bg-white">
+                {nav_options.map((option)=>(
+                  <Link to={`/${option.link}`}
+                    className="w-full text-center border-b border-black py-2 hover:bg-gray-200" key={option.name}>
+                    {option.name}
+                  </Link>
+                ))}
+                </div>
+            }
+
             {nav_options.map((option)=>(
-              <div className=" hidden md:block text-2xl" key={option}>
-                {option}
-              </div>
+              <Link to={`/${option.link}`}
+                className=" hidden md:block text-2xl" key={option.name}>
+                {option.name}
+              </Link>
             ))}
           </div>
         </div>
--- src/components/resturent.jsx
+++ src/components/resturent.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams,useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { setSingleResturent } from '../redux/features/resturentSlice';
 
+//icons imports
+import {AiOutlineArrowRight} from 'react-icons/ai'
+
 const Resturent = () => {
     const {id} = useParams();
+    const navigate = useNavigate();
+
     const {presented_resturent} = useSelector(store => store.resturentReducer);
     const dispatch = useDispatch();
+
+
     useEffect(() => {
         dispatch(setSingleResturent(id));
     },[]);
@@ -14,10 +21,15 @@ const Resturent = () => {
     
 
   return (
-    <div className='h-fit w-screen flex flex-wrap justify-center'>
+    <div className='h-fit w-screen flex flex-wrap justify-center relative'>
         <img 
             className='h-[30vh] w-screen'
             src={`${presented_resturent.main_image}`} alt="" />
+        <button 
+            onClick={()=>navigate(-1)}
+            className="text-white font-bold text-xl absolute top-2 right-4">
+            <AiOutlineArrowRight/>
+        </button>
         <div className=" w-screen flex flex-wrap justify-center h-[70vh]">
             <div className="w-[80vw] bg-white h-fit z-10 -mt-[5vh] p-8 rounded-md">
                 <p className='text-3xl border-b-2' >{presented_resturent.name}</p>
@@ -27,9 +39,11 @@ const Resturent = () => {
                  ))}
                 </p>
                 <p>{presented_resturent.description}</p>
-                <div className="grid grid-cols-2">
-                    {presented_resturent.images && presented_resturent.images.map((i)=>(
+                <p className=' font-bold w-full border-b my-1'>{presented_resturent.images && presented_resturent.images.length + " photos"}</p>
+                <div className="grid grid-cols-2 gap-1">
+                    {presented_resturent.images && presented_resturent.images.map((i,index)=>(
                         <img 
+                        key={index}
                         className='h-full w-full'
                         src={`${i}`} alt="" />
                     ))}
